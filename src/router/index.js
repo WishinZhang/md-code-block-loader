83180a64680645c5ffcd0a6f2c63b3905d73fadf
WishinZhang <wishin_zhang@163.com>
2020-04-13 22:27:59 +0800

    Operating system compatible

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Test from '../components/Test.md'
+import Test from '../components/Test-aaa.md'
 
 Vue.use(VueRouter)
 
